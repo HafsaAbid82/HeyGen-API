71ab2ee0be5cceac3a014f134e79774fbd658cd5
Hafsa Abid <hafsaabd.82@gmail.com>
2025-11-14 11:04:55 +0500

    Update AvatarGroup.py

--- AvatarGroup.py
+++ AvatarGroup.py
@@ -5,7 +5,7 @@ url = "https://api.heygen.com/v2/photo_avatar/avatar_group/create"
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
-    "x-api-key": "sk_V2_hgu_krGIQx04mA4_sdhZhRRAFPV3nLq5HVNx3uDWTS7swTNG"
+    "x-api-key": "<API KEY>"
 }
 payload = {
     "id": "36f9bca47f0c401faed22aa2d435b7f2",
