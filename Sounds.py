2f1d7cad557d3f2d0bd01310cce421a9a17ba7ea
Hafsa Abid <hafsaabd.82@gmail.com>
2025-11-14 11:03:42 +0500

    Update Sounds.py

--- Sounds.py
+++ Sounds.py
@@ -4,7 +4,7 @@ import json
 url = "https://api.heygen.com/v2/photo_avatar/add_sound_effect"
 headers = {
     "accept": "application/json",
-    "x-api-key": "sk_V2_hgu_krGIQx04mA4_sdhZhRRAFPV3nLq5HVNx3uDWTS7swTNG"
+    "x-api-key": ""<API KEY>""
 }
 payload = {  
      "id": "69e822924cc34dba9449d31e32ef0d63", 
